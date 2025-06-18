d9258637326d03214de47bd9a452dccf4783eb14
Oraya Chantanawat <120603568+orayachan@users.noreply.github.com>
2025-06-19 01:04:24 +0700

    chore: add exercise 12

---
+++ exercise12.js
@@ -0,0 +1,8 @@
+// Write an arrow function called greet that takes a single string argument, which is a person's name.
+
+const greet = (name) => {
+    return `Hey ${name}!`;
+};
+
+console.log(greet("Hagrid"));
+console.log(greet("Luna"));
